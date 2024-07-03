4daf03deb791ace71c92057f857e66dc53474ffc
moven0831 <moven0831@gmail.com>
2024-07-03 23:31:45 +0800

    chore: Add submodules and download script for repositories

---
+++ download_repos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/zama-ai/go-ethereum
+git clone https://github.com/zama-ai/evmos
+git clone https://github.com/zama-ai/ethermint
+git clone https://github.com/zama-ai/fhevm-requires-db
+git clone https://github.com/zama-ai/fhevm-hardhat-template
